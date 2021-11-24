1b2e8f224fcaeb7c2ccaac514bc6b85d550be8b2
daveycaviness <85713610+daveycaviness@users.noreply.github.com>
2021-11-23 22:59:00 -0500

    ToDo List Part 1

---
+++ Week1/toDoPart1/my-app/src/App.js
@@ -0,0 +1,13 @@
+import ToDoList from './ToDo.js'
+import './App.css';
+
+function App() {
+  return (
+   <div> 
+      <h1>ToDo List!</h1>
+      <ToDoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Week1/toDoPart1/my-app/src/ToDo.js
@@ -0,0 +1,21 @@
+export default function ToDoList(){
+    return(
+        <ul>
+            <li>
+                <input type= "checkbox"></input>
+                <label>Wash the Car!</label>
+            </li>
+            <li>
+                <input type= "checkbox"></input>
+                <label>Go grocery shopping!</label>
+            </li>
+            <li>
+                <input type= "checkbox"></input>
+                <label>Cut the Lawn!</label>
+            </li>
+
+        </ul>
+    );
+}
+
+ToDoList()
